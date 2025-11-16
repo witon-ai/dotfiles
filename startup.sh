d7bc5bb1bccb4eab8129f79c33b3184036c448a2
Witon AI <142131825+witon-ai@users.noreply.github.com>
2025-11-16 16:24:33 +0800

    Refactor .chezmoi.toml.tmpl to use keyring function for recipient retrieval and update startup.sh to use SERVICE variable for key management

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ export CHEZMOI_AGE_HOME="$CHEZMOI_HOME/age"
 export AGE_KEY_FILE_WITHOUT_POSTFIX="$CHEZMOI_AGE_HOME/key"
 export AGE_KEY_FILE="$AGE_KEY_FILE_WITHOUT_POSTFIX.txt"
 export AGE_KEY_PUBLIC_FILE="$AGE_KEY_FILE_WITHOUT_POSTFIX.pub"
-export ACCOUNT_NAME="chezmoi-age-public-key"
+export SERVICE="chezmoi-age-public-key"
 
 echo "Installing chezmoi..."
 brew install chezmoi
@@ -13,5 +13,4 @@ echo "Generating age key..."
 mkdir -p $CHEZMOI_AGE_HOME
 chezmoi age-keygen --output=$AGE_KEY_FILE
 age-keygen -y $AGE_KEY_FILE > $AGE_KEY_PUBLIC_FILE
-security delete-generic-password -a $ACCOUNT_NAME -s "age"
-security add-generic-password -a $ACCOUNT_NAME -s "age" -w "$(cat $AGE_KEY_PUBLIC_FILE)"
+chezmoi secret keyring set --service=$SERVICE --user=$USER --value="$(cat $AGE_KEY_PUBLIC_FILE)"
